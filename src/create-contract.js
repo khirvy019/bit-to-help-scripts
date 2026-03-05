aa8d065acfb47790e9f4bbd0ed030ca9978e9793
khirvy019 <khirvy_19@yahoo.com>
2026-03-05 08:30:37 +0800

    update create contract script

--- src/create-contract.js
+++ src/create-contract.js
@@ -4,24 +4,26 @@ import { decodeCashAddress } from '@bitauth/libauth';
 import MecenasArtifact from './contract/Mecenas.json' assert { type: 'json' };
 
 export function main() {
-  const recipientAddress = 'bitcoincash:....';
+  const recipientAddress = 'bchtest:qrjnxv8ndpqxlyjp3qm4rsns94pcy9s7n5j3j5kraz';
+  const funderAddress = 'bchtest:qrjnxv8ndpqxlyjp3qm4rsns94pcy9s7n5j3j5kraz';
   const period = 100;
-  const amountBch = 0.001;
+  const amountBch = 10000 / 10 ** 8;
 
   const contract = createContract(recipientAddress, period, amountBch);
   console.log("Contract Address:", contract.address);
 }
 
 
-export function createContract(recipientAddress, period, amountBch) {
+export function createContract(recipientAddress, funderAddress, period, amountBch) {
   const recipient = cashAddressToPkhash(recipientAddress);
+  const funder = cashAddressToPkhash(funderAddress);
   const amount = bchToSatoshis(amountBch);
 
   const network = 'chipnet'; // chipnet | mainnet
   const electrumNetworkProvider = new ElectrumNetworkProvider(network);
   const addressType = 'p2sh32'; // p2sh32 | p2sh20
 
-  const contractParameters = [recipient, BigInt(period), BigInt(amount)];
+  const contractParameters = [recipient, funder, BigInt(amount), BigInt(period)];
   const options = { provider: electrumNetworkProvider, addressType: addressType };
   const contract = new Contract(MecenasArtifact, contractParameters, options);
 
